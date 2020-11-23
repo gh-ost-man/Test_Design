e9625774c9a868b02c52892db44e2c7d5c57571e
lilyakaras <lilyca.hrupa@gmail.com>
2020-11-23 10:10:47 +0200

    Update

---
+++ TestLib/Test.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+
+namespace TestLib
+{
+
+    public class AnswerCL
+    {
+        public string Answer { get; set; }
+        public bool IsRight { get; set; }
+
+        public AnswerCL() { }
+
+        public AnswerCL(string answer, bool isRight)
+        {
+            this.Answer = answer;
+            this.IsRight = isRight;
+        }
+    }
+
+    public class QuestionCL
+    {
+        public string Question { get; set; }
+        public int Difficulty { get; set; }
+
+        public List<AnswerCL> Answers { get; set; }
+
+        public QuestionCL() { }
+
+        public QuestionCL(string question, List<AnswerCL> answers, int difficulty)
+        {
+            this.Question = question;
+            this.Answers = answers;
+            this.Difficulty = difficulty;
+        }
+    }
+
+    public class TestCL
+    {
+        public string Author { get; set; }
+        public List<QuestionCL> Questions { get; set; }
+
+        public TestCL() { }
+
+        public TestCL(string author, List<QuestionCL> questions)
+        {
+            this.Author = author;
+            this.Questions = questions;
+        }
+    }
+
+    public class ListTests
+    {
+        public List<TestCL> Tests = new List<TestCL>();
+
+        XmlSerializer xsListTestCL = new XmlSerializer(typeof(List<TestCL>), new XmlRootAttribute("Test"));
+        XmlSerializer xsTestCL = new XmlSerializer(typeof(List<TestCL>), new XmlRootAttribute("Test"));
+        XDocument doc;
+
+
+        public void SerializeXmlFileTest(string fileName, TestCL test)
+        {
+            using (FileStream fs = new FileStream("Tasks.xml", FileMode.OpenOrCreate))
+            {
+                xsTestCL.Serialize(fs, test);
+            }
+        }
+
+        public void DeserializeXmlFile(string path)
+        {
+            TestCL testCL;
+
+            using (FileStream fs = new FileStream("Tasks.xml", FileMode.OpenOrCreate))
+            {
+                testCL = (TestCL)xsTestCL.Deserialize(fs);
+            }
+        }
+
+    }
+
+}
--- Test_Design/Form1.Designer.cs
+++ Test_Design/Form1.Designer.cs
@@ -28,20 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btn_LoadTest = new System.Windows.Forms.Button();
+            this.btn_NewTest = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(437, 403);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // btn_LoadTest
+            // 
+            this.btn_LoadTest.Location = new System.Drawing.Point(490, 65);
+            this.btn_LoadTest.Name = "btn_LoadTest";
+            this.btn_LoadTest.Size = new System.Drawing.Size(75, 23);
+            this.btn_LoadTest.TabIndex = 1;
+            this.btn_LoadTest.Text = "Load test";
+            this.btn_LoadTest.UseVisualStyleBackColor = true;
+            // 
+            // btn_NewTest
+            // 
+            this.btn_NewTest.Location = new System.Drawing.Point(490, 12);
+            this.btn_NewTest.Name = "btn_NewTest";
+            this.btn_NewTest.Size = new System.Drawing.Size(75, 23);
+            this.btn_NewTest.TabIndex = 2;
+            this.btn_NewTest.Text = "New test";
+            this.btn_NewTest.UseVisualStyleBackColor = true;
+            this.btn_NewTest.Click += new System.EventHandler(this.btn_NewTest_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_NewTest);
+            this.Controls.Add(this.btn_LoadTest);
+            this.Controls.Add(this.dataGridView1);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btn_LoadTest;
+        private System.Windows.Forms.Button btn_NewTest;
     }
 }
 
